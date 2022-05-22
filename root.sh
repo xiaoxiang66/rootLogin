bff910358449dd5fc6b6a30a47a3b7feac98e8e7
xiaoxiang66 <61081156+xiaoxiang66@users.noreply.github.com>
2022-05-22 17:17:49 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -36,8 +36,8 @@ sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
 sudo lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
-$1= sshport
-$2= password
+$1 = sshport
+$2 = password
 
 
 sudo sed -i "s/^#\?Port.*/Port $sshport/g" /etc/ssh/sshd_config;
